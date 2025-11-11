166c55167a320c189fd72db19d12700a1019fd9a
josimarlife <josimar@olelife.com>
2025-11-11 15:40:58 -0500

    login succesfully

---
+++ cypress/e2e/spec.cy.ts
@@ -0,0 +1,11 @@
+describe('Login Sauce Demo', () => {
+  it('Login Succesfully', () => {
+    cy.visit('https://www.saucedemo.com/')
+
+    cy.get("#user-name").type("standard_user");
+    cy.get("#password").type("secret_sauce");
+    cy.get("#login-button").click();
+
+    cy.get('.title').should('have.text', 'Products');
+  })
+})
